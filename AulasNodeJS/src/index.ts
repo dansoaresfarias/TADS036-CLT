ed88cf7cdbf7243ffede0b906979b4f1b7466853
Danilo Farias <danilo.silva@nutes.ufpe.br>
2024-12-09 11:48:33 -0300

    Aula de NodeJS

--- AulasNodeJS/src/index.ts
+++ AulasNodeJS/src/index.ts
@@ -1,1 +1,17 @@
-console.log("Olá Mundo, sou a LocaDora!");
+import { Genero } from "./models/Genero";
+import { AppDataSource } from "./db/data-source";
+
+const generoTeste = new Genero("Terror");
+const genero2 = new Genero("Comedia");
+
+// console.log("Olá Mundo, sou a LocaDora!");
+//console.table(generoTeste);
+//console.table(genero2);
+
+AppDataSource.initialize()
+    .then(() => {
+        // here you can start to work with your database
+        console.log(`Database is running.`);
+    })
+    .catch((error) => console.log(error))   
+
